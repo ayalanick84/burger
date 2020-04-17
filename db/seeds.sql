4e357d84ae54c811ca38592feda192481645ad7a
Nicolas Ayala <ayalanick84@gmail.com>
2020-04-16 21:41:49 -0700

    Made the files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (name, devoured) VALUES ('Double Cheeseburger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Vegetarian', true);
+INSERT INTO burgers (name, devoured) VALUES ('Hawaiian', true);
